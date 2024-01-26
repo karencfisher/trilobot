5e5aa786fd9e4039acdd2d38026be97968d60e72
karencfisher <karencfisher@hotmail.com>
2024-01-26 03:35:34 -0700

    asynchronous control

--- control.py
+++ control.py
@@ -1,6 +1,39 @@
+from trilobot import *
+import os
+import time
 
 
+def dispatch_command(que, speed):
+    print(f"Start trilobot process {os.getpid()}")
+    tbot = Trilobot()
+    print(tbot, que)
+    while True:
+        if not que.empty():
+            tbot.set_button_led(2, True)
+            time.sleep(.1)
+            tbot.set_button_led(2, False)
+            time.sleep(.1)
 
-def dispatch_command(command):
-    return {'result': f'going {command}'}
+            command = que.get()
+            print(f'Command: {command}')
+            if command == "exit":
+                tbot.stop()
+                break
+            elif command == "forward":
+                tbot.forward(speed)
+            elif command == "reverse":
+                tbot.backward(speed)
+            elif command == "left":
+                tbot.turn_left(speed)
+            elif command == "right":
+                tbot.turn_right(speed)
+            else:
+                tbot.stop()
+        else:
+            tbot.set_button_led(3, True)
+            time.sleep(.1)
+            tbot.set_button_led(3, False)
+            time.sleep(.1)
+
+    print("End trilobot process")
 
--- static/control.js
+++ static/control.js
@@ -1,42 +1,11 @@
-const up_arrow = document.getElementById("up");
-const left_arrow = document.getElementById("left");
-const stop_button = document.getElementById("stop");
-const right_arrow = document.getElementById("right");
-const down_arrow = document.getElementById("down");
-const status_div = document.getElementById("status");
-const base_url = window.location.origin + window.location.pathname
+const buttons = document.getElementsByClassName("direction")
+const statusDiv = document.getElementById("status");
+const baseUrl = window.location.origin + window.location.pathname
 
-up_arrow.addEventListener("click", (e) => {
-    const url = base_url + "controls?command=forward";
-    d3.json(url).then(data => {
-        status_div.innerHTML = data;
-    })
-});
+for (let button of buttons) {
+    button.addEventListener("click", (e) => {
+        const url = `${baseUrl}controls?command=${e.target.id}`;
+        d3.json(url).then(data => statusDiv.innerHTML = data);
+    });
+}
 
-left_arrow.addEventListener("click", (e) => {
-    const url = base_url + "controls?command=left";
-    d3.json(url).then(data => {
-        status_div.innerHTML = data;
-    })
-});
-
-stop_button.addEventListener("click", (e) => {
-    const url = base_url + "controls?command=stop";
-    d3.json(url).then(data => {
-        status_div.innerHTML = data;
-    })
-});
-
-right_arrow.addEventListener("click", (e) => {
-    const url = base_url + "controls?command=right";
-    d3.json(url).then(data => {
-        status_div.innerHTML = data;
-    })
-});
-
-down_arrow.addEventListener("click", (e) => {
-    const url = base_url + "controls?command=reverse";
-    d3.json(url).then(data => {
-        status_div.innerHTML = data;
-    })
-});
